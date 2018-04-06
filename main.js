0ba09616c186a3106f56389aacf3a27cfa735493
Jason Yu <jasonyu@openrice.com>
2018-04-06 18:23:21 +0800

    Testing chinese

--- main.js
+++ main.js
@@ -1,18 +1,46 @@
-// Imports the Google Cloud client library
-const vision = require('@google-cloud/vision');
+function detectFulltext(fileName) {
+    // [START vision_fulltext_detection]
 
-// Creates a client
-const client = new vision.ImageAnnotatorClient();
+    // Imports the Google Cloud client library
+    const vision = require('@google-cloud/vision');
 
-// Performs label detection on the image file
-client
-  .labelDetection('./resources/wakeupcat.jpg')
-  .then(results => {
-    const labels = results[0].labelAnnotations;
+    // Creates a client
+    const client = new vision.ImageAnnotatorClient();
 
-    console.log('Labels:');
-    labels.forEach(label => console.log(label.description));
-  })
-  .catch(err => {
-    console.error('ERROR:', err);
-  });
+    /**
+     * TODO(developer): Uncomment the following line before running the sample.
+     */
+    // const fileName = 'Local image file, e.g. /path/to/image.png';
+
+    // Read a local image as a text document
+    client
+        .documentTextDetection(fileName)
+        .then(results => {
+        const fullTextAnnotation = results[0].fullTextAnnotation;
+        console.log(`Full text: ${fullTextAnnotation.text}`);
+
+        fullTextAnnotation.pages.forEach(page => {
+            page.blocks.forEach(block => {
+            console.log(`Block confidence: ${block.confidence}`);
+            block.paragraphs.forEach(paragraph => {
+                console.log(`Paragraph confidence: ${paragraph.confidence}`);
+                paragraph.words.forEach(word => {
+                const wordText = word.symbols.map(s => s.text).join('');
+                console.log(`Word text: ${wordText}`);
+                console.log(`Word confidence: ${word.confidence}`);
+                word.symbols.forEach(symbol => {
+                    console.log(`Symbol text: ${symbol.text}`);
+                    console.log(`Symbol confidence: ${symbol.confidence}`);
+                });
+                });
+            });
+            });
+        });
+        })
+        .catch(err => {
+        console.error('ERROR:', err);
+        });
+    // [END vision_fulltext_detection]
+}
+
+detectFulltext('./resources/test-chinese.jpg');
